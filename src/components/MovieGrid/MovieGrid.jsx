88ba7d16b4719e3bda5585eaffb488b2c75726f3
Max Zarev <maksimelizarev@gmail.com>
2025-10-19 18:52:05 +0500

    Refactor MovieGrid component: replace hardcoded MovieCard instances with a dynamic data array, adding unique IDs for each movie.

--- src/components/MovieGrid/MovieGrid.jsx
+++ src/components/MovieGrid/MovieGrid.jsx
@@ -2,64 +2,79 @@ import './MovieGrid.css';
 import MovieCard from './../MovieCard/MovieCard';
 
 export default function MovieGrid() {
+
+  const data = [
+    {
+      img: "./black-widow.png",
+      title: "Black Widow",
+      link: "#",
+      rating: "324",
+      isLike: false,
+    },
+    {
+      img: "./Shang Chi.jpg",
+      title: "Shang Chi",
+      link: "#",
+      rating: "124",
+      isLike: false
+    },
+    {
+      img: "./Loki.jpg",
+      title: "Loki",
+      link: "#",
+      rating: "235",
+      isLike: false
+    },
+    {
+      img: "./How I Met Your Mother.jpg",
+      title: "How I Met Your Mother",
+      link: "#",
+      rating: "123",
+      isLike: false
+    },
+    {
+      img: "./Money Heist.jpg",
+      title: "Money Heist",
+      link: "#",
+      rating: "8125",
+      isLike: true
+    },
+    {
+      img: "./Friends.jpg",
+      title: "Friends",
+      link: "#",
+      rating: "123",
+      isLike: false
+    },
+    {
+      img: "./The Big Bang Theory.jpg",
+      title: "The Big Bang Theory",
+      link: "#",
+      rating: "12",
+      isLike: false
+    },
+    {
+      img: "./Two And a Half Men.jpg",
+      title: "Two And a Half Men",
+      link: "#",
+      rating: "456",
+      isLike: false
+    }
+  ]
+  data.map(el => el.id = crypto.randomUUID())
+  
   return (
     <div className="grid-container">
-      <MovieCard
-        img="./black-widow.png"
-        title="Black Widow"
-        link="#"
-        rating="324"
-        isLike={false}
-      />
-      <MovieCard
-        img="./Shang Chi.jpg"
-        title="Shang Chi"
-        link="#"
-        rating="124"
-        isLike={false}
-      />
-      <MovieCard
-        img="./Loki.jpg"
-        title="Loki"
-        link="#"
-        rating="235"
-        isLike={false}
-      />
-      <MovieCard
-        img="./How I Met Your Mother.jpg"
-        title="How I Met Your Mother"
-        link="#"
-        rating="123"
-        isLike={false}
-      />
-      <MovieCard
-        img="./Money Heist.jpg"
-        title="Money Heist"
-        link="#"
-        rating="8125"
-        isLike={true}
-      />
-      <MovieCard
-        img="./Friends.jpg"
-        title="Friends"
-        link="#"
-        rating="123"
-        isLike={false}
-      />
-      <MovieCard
-        img="./The Big Bang Theory.jpg"
-        title="The Big Bang Theory"
-        link="#"
-        rating="12"
-        isLike={false}
-      />
-      <MovieCard
-        img="./Two And a Half Men.jpg"
-        title="Two And a Half Men"
-        link="#"
-        rating="456"
-        isLike={false}
-      />
+      {data.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          img={movie.img}
+          title={movie.title}
+          link={movie.link}
+          rating={movie.rating}
+          isLike={movie.isLike}
+        />
+      ))}
     </div>
   );
 }
